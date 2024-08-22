e67bfb3c15160888aadb903bcc4db32d40d02a53
ayushsingh82 <ayushsinghmi711@gmail.com>
2024-08-22 12:53:43 +0530

    init

---
+++ client/tailwind.config.js
@@ -0,0 +1,17 @@
+const {
+  default: flattenColorPalette,
+} = require("tailwindcss/lib/util/flattenColorPalette");
+
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  darkMode: "class",
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
