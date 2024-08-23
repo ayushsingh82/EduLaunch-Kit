1cc05724d5ec06d125b51973bbcb67cf8d9995e7
ayushsingh82 <ayushsinghmi711@gmail.com>
2024-08-23 15:29:38 +0530

    navbar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -10,20 +10,20 @@ const gradientStyle = {
 const Navbar = () => {
   return (
     <nav className="sticky top-0 z-50">
-      <div className="flex flex-row mx-auto px-[40px] py-[20px] justify-between items-center bg-blue-600">
+      <div className="flex flex-row mx-auto px-[40px] py-[20px] justify-between items-center bg-black">
         <div className="flex flex-row items-center font-bold text-2xl text-white">
           <a href="/">EDU</a>
         </div>
 
         <div className="flex-1 flex justify-center items-center space-x-8">
           <h1 className="font-medium text-xxl text-white">
-            <a href="/organisations">Organisations</a>
+            <a href="/organisations">Features</a>
           </h1>
           <h1 className="font-medium text-xxl text-white">
-            <a href="">Courses</a>
+            <a href="">NFT </a>
           </h1>
           <h1 className="font-medium text-xxl text-white">
-            <a href="">Certificates</a>
+            <a href="">Defi</a>
           </h1>
           <h1 className="font-medium text-xxl text-white">
             <a href="/landing">Join Us</a>
