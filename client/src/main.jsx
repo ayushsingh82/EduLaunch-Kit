ec0cbddcf455271ce44fa8fb240cafbd31bd9196
ayushsingh82 <ayushsinghmi711@gmail.com>
2024-08-23 16:06:18 +0530

    wallet

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -13,6 +13,33 @@ import {
 } from "react-router-dom";
 import { Route } from "react-router-dom";
 
+import '@rainbow-me/rainbowkit/styles.css';
+import {
+  getDefaultConfig,
+  RainbowKitProvider,
+} from '@rainbow-me/rainbowkit';
+import { WagmiProvider } from 'wagmi';
+import {
+  mainnet,
+  polygon,
+  optimism,
+  arbitrum,
+  base,
+} from 'wagmi/chains';
+import {
+  QueryClientProvider,
+  QueryClient,
+} from "@tanstack/react-query";
+
+const config = getDefaultConfig({
+  appName: 'My RainbowKit App',
+  projectId: 'e7fa7d19fd057ecd9403a0e89bd62b8b',
+  chains: [mainnet, polygon, optimism, arbitrum, base],
+  ssr: false
+});
+
+const queryClient = new QueryClient();
+
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route>
@@ -24,7 +51,13 @@ const router = createBrowserRouter(
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+  <WagmiProvider config={config}>
+  <QueryClientProvider client={queryClient}>
+  <RainbowKitProvider>
   <Navbar />
   <RouterProvider router={router} />
+  </RainbowKitProvider>
+  </QueryClientProvider>
+  </WagmiProvider>
   </StrictMode>,
 )
